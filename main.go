9512ee36e1b992e993f0a186f12a1167ca2cd228
sy14r <sy14r@localhost>
2020-01-07 13:23:03 -0700

    hashwrap push

--- main.go
+++ main.go
@@ -6,29 +6,34 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
+	"strconv"
 	"sync"
 	"time"
 )
 
 func main() {
-	cmd := exec.Command(os.Args[1], os.Args[2:]...)
+	intervalTime, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatalf("Invalid interval provided")
+	}
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 
-	var stdoutBuf, stderrBuf bytes.Buffer
+	// var stdoutBuf, stderrBuf bytes.Buffer
+	var stdoutBuf bytes.Buffer
 	stdoutIn, _ := cmd.StdoutPipe()
-	stderrIn, _ := cmd.StderrPipe()
+	//stderrIn, _ := cmd.StderrPipe()
 	stdinIn, _ := cmd.StdinPipe()
 
-	var errStdout, errStderr error
+	// var errStdout error
 	stdout := io.MultiWriter(&stdoutBuf)
 	// stdout := io.MultiWriter(os.Stdout, &stdoutBuf)
-	stderr := io.MultiWriter(os.Stderr, &stderrBuf)
-	err := cmd.Start()
+	//stderr := io.MultiWriter(os.Stderr, &stderrBuf)
+	err = cmd.Start()
 	if err != nil {
 		log.Fatalf("cmd.Start() failed with '%s'\n", err)
 	}
@@ -39,7 +44,8 @@ func main() {
 
 	// Go func to get stdout passed through and finish the WaitGroup when exe is done
 	go func() {
-		_, errStdout = io.Copy(stdout, stdoutIn)
+		// _, errStdout = io.Copy(stdout, stdoutIn)
+		io.Copy(stdout, stdoutIn)
 		wg.Done()
 	}()
 
@@ -48,27 +54,30 @@ func main() {
 		defer stdinIn.Close()
 		sum := 1
 		for sum < 2 {
-			stdoutBuf.Reset()
-			time.Sleep(5 * time.Second)
+			// in loop will want to check for tasking from s3 (can manage this with an external script and just have this look for the local file of hashcat.externalsignal and process that as needed?) - this is a future development
 			io.WriteString(stdinIn, "s")
 			//data := strings.Split(stdoutBuf.String(), "\n")
 			d1 := stdoutBuf.Bytes()
 			ioutil.WriteFile("./hashcat.status", d1, 0644)
 			//fmt.Println()
+			stdoutBuf.Reset()
+			time.Sleep(time.Duration(intervalTime) * time.Second)
 		}
 	}()
 
-	_, errStderr = io.Copy(stderr, stderrIn)
+	//_, errStderr = io.Copy(stderr, stderrIn)
 	wg.Wait()
 
 	err = cmd.Wait()
-	if err != nil {
-		log.Fatalf("cmd.Run() failed with %s\n", err)
-	}
-	if errStdout != nil || errStderr != nil {
-		log.Fatal("failed to capture stdout or stderr\n")
-	}
+	// if err != nil {
+	// 	log.Fatalf("cmd.Run() failed with %s\n", err)
+	// }
+	// if errStdout != nil || errStderr != nil {
+	// 	log.Fatal("failed to capture stdout or stderr\n")
+	// }
 
-	outStr, errStr := string(stdoutBuf.Bytes()), string(stderrBuf.Bytes())
-	fmt.Printf("\nout:\n%s\nerr:\n%s\n", outStr, errStr)
+	// outStr, errStr := string(stdoutBuf.Bytes()), string(stderrBuf.Bytes())
+	// outStr, errStr := string(stdoutBuf.Bytes()), string(stderrBuf.Bytes())
+	// fmt.Printf("\nout:\n%s\nerr:\n%s\n", outStr, errStr)
+	ioutil.WriteFile("./hashcat.status", stdoutBuf.Bytes(), 0644)
 }
